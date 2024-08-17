da199e7c445eb50581ff890366bac24922fa7b64
Nidhay Reddy <96610602+vinkar-89@users.noreply.github.com>
2024-08-17 22:31:52 +0530

    Add files via upload

---
+++ profile_screen.dart
@@ -0,0 +1,13 @@
+import 'backend.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Icon(Icons.monitor_heart_rounded),
+    );
+  }
+}
